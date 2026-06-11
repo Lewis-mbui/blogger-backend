8c1392e410ac10489600b0c1fc92880cc176ee6d
Lewis-mbui <lewmugo14479@gmail.com>
2026-06-11 03:50:52 +0300

    fix: route error handler

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -2,10 +2,8 @@ module.exports = function(err, req, res, next) {
   console.error(err.message, err);
 
   if (err.message === "Email already in use") {
-    response.json({message: err.message})
+    res.status(409).json({message: err.message})
   } else {
     res.status(500).json({message: "something failed"});
   }
-
-  process.exit(1);
 }
