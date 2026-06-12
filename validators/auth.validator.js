b12519dc7f8015202cb12d301b67e8dba93cecb4
Lewis-mbui <lewmugo14479@gmail.com>
2026-06-12 13:05:58 +0300

    refactor: auth schema

--- validators/auth.validator.js
+++ validators/auth.validator.js
@@ -1,17 +1,6 @@
 const Joi = require('joi');
 
 const authSchema = Joi.object({
-  name: Joi.string()
-    .min(2)
-    .max(50)
-    .required()
-    .trim()
-    .messages({
-      "string.base": "Name must be a string",
-      "string.min": "Name must be at least 2 characters",
-      "string.max": "Name must be at most 50 characters",
-      "any.required": "Name is required",
-    }),
   email: Joi.string()
     .email()
     .min(5)
