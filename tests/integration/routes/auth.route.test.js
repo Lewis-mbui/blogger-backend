09931f7f485f05165817025660dd37f4c1e25a9f
Lewis-mbui <lewmugo14479@gmail.com>
2026-06-17 12:54:21 +0300

    refactor: auth route test

--- tests/integration/routes/auth.route.test.js
+++ tests/integration/routes/auth.route.test.js
@@ -7,153 +7,150 @@ const {
 const app = require('../../../startup/app');
 const User = require('../../../models/User');
 
-describe('/api/auth', () => {
+describe('POST /api/auth', () => {
   beforeAll(async () => await connectDatabase());
   afterEach(async () =>  await clearDatabase());
   afterAll(async () => await closeDatabase());
 
-  describe('POST /', () => {
-    let email;
-    let password;
+  let email;
+  let password;
 
-    const exec = () => {
-      return request(app)
-        .post('/api/auth')
-        .send({email, password});
-    }
+  const exec = () => {
+    return request(app)
+      .post('/api/auth')
+      .send({email, password});
+  }
 
-    beforeEach(async () => {
-      const user = new User({name: 'aaa', email: 'a@mail.com', password: 'Password123!'});
-      await user.save();
+  beforeEach(async () => {
+    const user = new User({name: 'aaa', email: 'a@mail.com', password: 'Password123!'});
+    await user.save();
 
-      email = "a@mail.com";
-      password = 'Password123!';
-    });
+    email = "a@mail.com";
+    password = 'Password123!';
+  });
 
-    it('should return 400 if email is missing from request', async() => {
-      email = undefined;
+  it('should return 400 if email is missing from request', async() => {
+    email = undefined;
 
-      const res = await exec();
-      const {details} = res.body;
+    const res = await exec();
+    const {details} = res.body;
 
-      expect(res.status).toBe(400);
-      expect(details[0].message).toMatch(/.*email.*/i);
-    });
+    expect(res.status).toBe(400);
+    expect(details[0].message).toMatch(/.*email.*/i);
+  });
 
-    it('should return 400 if email is not a valid email', async () => {
-      email = 'abcdef';
+  it('should return 400 if email is not a valid email', async () => {
+    email = 'abcdef';
 
-      const res = await exec();
-      
-      expect(res.status).toBe(400);
-      expect(res.body.details[0].message).toMatch(/.*email.*/i);
-    });
+    const res = await exec();
+    
+    expect(res.status).toBe(400);
+    expect(res.body.details[0].message).toMatch(/.*email.*/i);
+  });
 
-    it('should return 400 if password is missing', async() => {
-      password = undefined;
+  it('should return 400 if password is missing', async() => {
+    password = undefined;
 
-      const res = await exec();
-      const {details} = res.body;
+    const res = await exec();
+    const {details} = res.body;
 
-      expect(res.status).toBe(400);
-    });
+    expect(res.status).toBe(400);
+  });
 
-    it('should return 400 if password is missing', async() => {
-      password = undefined;
+  it('should return 400 if password is missing', async() => {
+    password = undefined;
 
-      const res = await exec();
-      const {details} = res.body;
+    const res = await exec();
+    const {details} = res.body;
 
-      expect(res.status).toBe(400);
-    });
+    expect(res.status).toBe(400);
+  });
 
-    it('should return 400 if password is more than 100 characters', async () => {
-      password = new Array(90).join('a') + 'Password123!'; // total 101 characters
+  it('should return 400 if password is more than 100 characters', async () => {
+    password = new Array(90).join('a') + 'Password123!'; // total 101 characters
 
-      const res = await exec();
+    const res = await exec();
 
-      expect(res.status).toBe(400);
-      expect(res.body.details[0].message).toMatch(/.*password.*/i);
-    });
+    expect(res.status).toBe(400);
+    expect(res.body.details[0].message).toMatch(/.*password.*/i);
+  });
 
-    it('should return 400 if password is less than 8 characters', async () => {
-      password = "Pass1!";
+  it('should return 400 if password is less than 8 characters', async () => {
+    password = "Pass1!";
 
-      const res = await exec();
+    const res = await exec();
 
-      expect(res.status).toBe(400);
-      expect(res.body.details[0].message).toMatch(/.*password.*/i);
-    });
+    expect(res.status).toBe(400);
+    expect(res.body.details[0].message).toMatch(/.*password.*/i);
+  });
 
-     it('should return 400 if password does not have a lowercase letter', async () => {
-      password = "PASSWORD123!";
+    it('should return 400 if password does not have a lowercase letter', async () => {
+    password = "PASSWORD123!";
 
-      const res = await exec();
+    const res = await exec();
 
-      expect(res.status).toBe(400);
-      expect(res.body.details[0].message).toMatch(/.*password.*/i);
-    });
+    expect(res.status).toBe(400);
+    expect(res.body.details[0].message).toMatch(/.*password.*/i);
+  });
 
-     it('should return 400 if password does not have an uppercase letter', async () => {
-      password = "password123!";
+    it('should return 400 if password does not have an uppercase letter', async () => {
+    password = "password123!";
 
-      const res = await exec();
+    const res = await exec();
 
-      expect(res.status).toBe(400);
-      expect(res.body.details[0].message).toMatch(/.*password.*/i);
-    });
+    expect(res.status).toBe(400);
+    expect(res.body.details[0].message).toMatch(/.*password.*/i);
+  });
 
-    it('should return 400 if password does not have a number', async () => {
-      password = "Password!";
+  it('should return 400 if password does not have a number', async () => {
+    password = "Password!";
 
-      const res = await exec();
+    const res = await exec();
 
-      expect(res.status).toBe(400);
-      expect(res.body.details[0].message).toMatch(/.*password.*/i);
-    });
+    expect(res.status).toBe(400);
+    expect(res.body.details[0].message).toMatch(/.*password.*/i);
+  });
 
-    it('should return 400 if user is not registered', async () => {
-      email = 'b@mail.com';
+  it('should return 400 if user is not registered', async () => {
+    email = 'b@mail.com';
 
-      const res = await exec();
+    const res = await exec();
 
-      expect(res.status).toBe(400);
-    });
+    expect(res.status).toBe(400);
+  });
 
-    it('should return 400 if password is incorrect', async () => {
-      password = 'PassWord123!';
+  it('should return 400 if password is incorrect', async () => {
+    password = 'PassWord123!';
 
-      const res = await exec();
+    const res = await exec();
 
-      expect(res.status).toBe(400);
-    });
+    expect(res.status).toBe(400);
+  });
 
-    it('should return a user if request is valid', async () => {
-      const res = await exec();
+  it('should return a user if request is valid', async () => {
+    const res = await exec();
 
-      const {data} = res.body;
+    const {data} = res.body;
 
-      expect(res.status).toBe(200);
-      expect(data).toHaveProperty('user');
-      expect(data.user).toHaveProperty('name', 'aaa');
-    });
+    expect(res.status).toBe(200);
+    expect(data).toHaveProperty('user');
+    expect(data.user).toHaveProperty('name', 'aaa');
+  });
 
-    it('should return a token if request is valid', async () => {
-      const res = await exec();
+  it('should return a token if request is valid', async () => {
+    const res = await exec();
 
-      const {data} = res.body;
+    const {data} = res.body;
 
-      expect(data).toHaveProperty('token');
-      expect(data.token).toBeDefined();
-    });
+    expect(data).toHaveProperty('token');
+    expect(data.token).toBeDefined();
+  });
 
-    it('should not return password if request is valid', async () => {
-      const res = await exec();
+  it('should not return password if request is valid', async () => {
+    const res = await exec();
 
-      const {data} = res.body;
+    const {data} = res.body;
 
-      expect(data.user).not.toHaveProperty('password');
-    });
-
+    expect(data.user).not.toHaveProperty('password');
   });
 });
